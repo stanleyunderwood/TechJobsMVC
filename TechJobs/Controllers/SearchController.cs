e899f643d57c6a07c63e0f03aeb099d93e862f12
stanleyunderwood <33239531+stanleyunderwood@users.noreply.github.com>
2018-03-01 16:59:40 -0600

    Final changes

---
+++ TechJobs/Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            return View();
+        }
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            ViewBag.title = "Search";
+            ViewBag.searchTerm = searchTerm;
+            ViewBag.columns = ListController.columnChoices;
+
+            if (searchType == "all")
+            {
+                ViewBag.jobs = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                ViewBag.jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            return View("Index");
+        }
+    }
+}
